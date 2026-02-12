2bc5a1811ab85b8824e2afaa84e4fff81386dbf0
Ma. Pauline Gonzales <gpauline61@gmail.com>
2026-02-12 09:32:39 +0800

    dfgdfg

--- MMSWebApp2/obj/Debug/net10.0/MMSWebApp2.AssemblyInfo.cs
+++ MMSWebApp2/obj/Debug/net10.0/MMSWebApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MMSWebApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224b7198fbf7785e74b42aff7b18f6a6c5c19a0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4296e0082c582d5f967697c251942fd5cb6272ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MMSWebApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MMSWebApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
